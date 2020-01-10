8345a3cc2866b20a5cc161a3f45eef3cc6bd5e2b
Sean Doyle <sean.p.doyle24@gmail.com>
2020-01-10 10:09:32 -0500

    Render the Cart
    Once a `Book` record is in a visitor's `Order`, the corresponding
    `LineItem` record is rendered into the page's cart.
    
    To render the cart, and to handle the expanding and collapsing of the
    cart, render a [`<details>`][details] element in the page's `<nav>`,
    containing an SVG icon in the child [`<summary>`][summary] element.
    
    To simplify the interactions with the cart in the System Test suite,
    introduce the `expand_cart` test helper, which invokes [`find`][find]
    with the `text:` option containing the translated `<title>` text we
    embed into the cart icon's `inline_svg_tag`.
    
    [details]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details
    [summary]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/summary
    [find]: https://www.rubydoc.info/github/jnicklas/capybara/master/Capybara/Node/Finders#find-instance_method

--- test/system/visitor_adds_book_to_cart_test.rb
+++ test/system/visitor_adds_book_to_cart_test.rb
@@ -2,16 +2,23 @@ require "application_system_test_case"
 
 class VisitorAddsBookToCartTest < ApplicationSystemTestCase
   test "visitor adds a Book to their Cart" do
-    book = books(:ruby_science)
+    ruby_science = books(:ruby_science)
 
     visit root_path
-    click_on book.title
-    click_on translate(:create, scope: "helpers.submit.line_item")
+    click_on ruby_science.title
+    click_on translate("helpers.submit.line_item.create")
 
-    assert_text added_book_flash(book)
+    assert_text added_book_flash(ruby_science)
+    within cart.tap(&:click) do
+      assert_book ruby_science
+    end
   end
 
   def added_book_flash(book)
     translate("line_items.create.notice", title: book.title)
+  end
+
+  def cart
+    find("details", text: translate("layouts.application.cart"), visible: false)
   end
 end
