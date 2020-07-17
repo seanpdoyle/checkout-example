c288604e866690b2ccb8f8ceb53ffac5e7caacca
Sean Doyle <sean.p.doyle24@gmail.com>
2020-07-17 17:05:15 -0400

    Upload and render Book Cover images

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   has_rich_text :description
   has_rich_text :table_of_contents
 
+  has_one_attached :cover
+
   def price_in_dollars
     price_in_cents / 100.0
   end
