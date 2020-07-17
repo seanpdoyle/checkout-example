b9b55db05e1f57299be31b9631904c5269e872a7
Sean Doyle <sean.p.doyle24@gmail.com>
2020-07-17 16:09:36 -0400

    Navigate back to Library from Book's page
    Original error
    
    ```
    Error:
    VisitorNavigatesBackToLibraryFromABookTest#test_visitor_navigates_back_to_library_from_a_book:
        Capybara::ElementNotFound: Unable to find link or button "<span class=\"translation_missing\" title=\"translation missing: en.books.show.library\">Library</span>"
    ```
    
    More strict error
    
    ```
    Error:
      VisitorNavigatesBackToLibraryFromABookTest#test_visitor_navigates_back_to_library_from_a_book:
      I18n::MissingTranslationData: translation missing: en.books.show.library
    ```

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,11 +17,11 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
@@ -45,5 +45,5 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 end
