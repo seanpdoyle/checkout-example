85d4129d9401a8adb2880f9b5d3fb647af9c96e1
Sean Doyle <sean.p.doyle24@gmail.com>
2020-07-17 15:43:05 -0400

    Render Rich Text on a Book's page
    [@tailwindcss/typography]: https://github.com/tailwindcss/typography/tree/v0.2.0#size-modifiers

--- test/system/visitor_views_a_book_test.rb
+++ test/system/visitor_views_a_book_test.rb
@@ -8,5 +8,11 @@ class VisitorViewsABookTest < ApplicationSystemTestCase
     click_on ruby_science.title
 
     assert_book ruby_science
+    assert_rich_text ruby_science.description
+    assert_rich_text ruby_science.table_of_contents
+  end
+
+  def assert_rich_text(rich_text)
+    assert_includes page.html, rich_text.body.to_html
   end
 end
