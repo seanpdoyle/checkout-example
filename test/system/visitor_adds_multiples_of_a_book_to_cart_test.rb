4a63a5f8a465fb6c329f05cd55585e0b157e0818
Sean Doyle <sean.p.doyle24@gmail.com>
2020-07-22 19:30:41 -0400

    Add a quantity of Books to the Cart
    In JavaScript-less environments, hide the button and rely on the `<input
    type="number">`-provided step buttons.
    
    Once JavaScript evaluates the `input-step` controller's `connect()`
    hook, hide the browser default buttons and remove the `<button>`
    elements' `hidden` attribute.
    
    [data-action]: https://stimulusjs.org/reference/actions
    [stepUp]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/stepUp
    [stepDown]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/stepDown

---
+++ test/system/visitor_adds_multiples_of_a_book_to_cart_test.rb
@@ -0,0 +1,32 @@
+require "application_system_test_case"
+
+class VisitorAddsMultiplesOfABookToCartTest < ApplicationSystemTestCase
+  test "visitor adds a Book multiple times to the cart" do
+    ruby_science = books(:ruby_science)
+
+    visit book_path(ruby_science)
+    3.times { click_on translate("helpers.submit.line_item.increment") }
+    2.times { click_on translate("helpers.submit.line_item.decrement") }
+    click_on translate("helpers.submit.line_item.create")
+    expand_cart
+
+    within cart do
+      assert_book ruby_science
+      assert_total ruby_science.price_in_dollars * 2
+    end
+  end
+
+  test "visitor adds a quantity of Books to the cart" do
+    ruby_science = books(:ruby_science)
+
+    visit book_path(ruby_science)
+    fill_in translate("helpers.label.line_item.increment"), with: 3
+    click_on translate("helpers.submit.line_item.create")
+    expand_cart
+
+    within cart do
+      assert_book ruby_science
+      assert_total ruby_science.price_in_dollars * 3
+    end
+  end
+end
