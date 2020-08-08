f580eb59f4739837bf2ba4551163f683ce6f258c
Sean Doyle <sean.p.doyle24@gmail.com>
2020-08-07 23:21:48 -0400

    extract common ancestor, share partials

--- app/controllers/checkouts/payments_controller.rb
+++ app/controllers/checkouts/payments_controller.rb
@@ -1,6 +1,9 @@
 module Checkouts
   class PaymentsController < ApplicationController
     def new
+      payment = Payment.find(params[:order_id])
+
+      render locals: {payment: payment}
     end
   end
 end
