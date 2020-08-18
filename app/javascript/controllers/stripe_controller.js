6056bdedbaadf123c8b8525cab02f47aec6a9e05
Sean Doyle <sean.p.doyle24@gmail.com>
2020-08-18 17:00:18 -0400

    [COME BACK] Stripe JS Errors
    [test-card-numbers]: https://stripe.com/docs/testing#cards-responses

--- app/javascript/controllers/stripe_controller.js
+++ app/javascript/controllers/stripe_controller.js
@@ -4,8 +4,11 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [
     "cardNumber",
+    "cardNumberErrors",
     "cardExpiry",
+    "cardExpiryErrors",
     "cardCvc",
+    "cardCvcErrors",
     "paymentMethod",
   ]
   static values = {
@@ -35,13 +38,42 @@ export default class extends Controller {
     this.cardNumber.unmount()
   }
 
+  dispatch(eventName, { target = this.element, detail = {}, bubbles = true, cancelable = true } = {}) {
+    const type = `${this.identifier}:${eventName}`
+    const event = new CustomEvent(type, { detail, bubbles, cancelable })
+    target.dispatchEvent(event)
+
+    return event
+  }
+
+  clearErrors() {
+    this.cardExpiryErrorsTarget.innerHTML = ""
+    this.cardCvcErrorsTarget.innerHTML = ""
+    this.cardNumberErrorsTarget.innerHTML = ""
+  }
+
+  renderError(event) {
+    const { type, code, message } = event.detail
+    let target = this.cardNumberErrorsTarget
+
+    if (code.match(/expiry/i) || code.match(/expired/i)) {
+      target = this.cardExpiryErrorsTarget
+    } else if (code.match(/cvc/i)) {
+      target = this.cardCvcErrorsTarget
+    }
+
+    if (["card_error", "validation_error"].includes(type)) {
+      target.innerHTML = message
+    }
+  }
+
   async confirmCardPayment(event) {
     if (this.paymentMethodTarget.value) {
     } else {
       event.preventDefault()
       event.stopPropagation()
 
-      const { paymentIntent } = await this.stripe.confirmCardPayment(
+      const { paymentIntent, error } = await this.stripe.confirmCardPayment(
         this.clientSecretValue, {
           payment_method: {
             card: this.elements.getElement("cardNumber"),
@@ -54,6 +86,8 @@ export default class extends Controller {
         this.paymentMethodTarget.value = paymentIntent.payment_method
 
         this.element.requestSubmit()
+      } else {
+        this.dispatch("error", { detail: error })
       }
     }
   }
--- test/system/visitor_checks_out_order_test.rb
+++ test/system/visitor_checks_out_order_test.rb
@@ -43,10 +43,55 @@ class VisitorChecksOutOrderTest < ApplicationSystemTestCase
     assert_total shipment.total_in_dollars
   end
 
-  def checkout
-    expand_cart
+  test "visitor is notified when a payment submission is declined" do
+    shipment = shipments(:shipment_rails)
 
-    click_on translate("orders.order.checkout")
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0002
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_number), "Your card was declined."
+  end
+
+  test "visitor is notified when a payment submission has an expired card" do
+    shipment = shipments(:shipment_rails)
+
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0069
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_expiry), "Your card has expired."
+  end
+
+  test "visitor is notified when a payment submission's card verification check fails" do
+    shipment = shipments(:shipment_rails)
+
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0101
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_cvc), "Your card's security code is incorrect."
+  end
+
+  def checkout
+    expand_cart do
+      click_on translate("orders.order.checkout")
+    end
   end
 
   def submit_shipment_form(shipment)
@@ -60,11 +105,25 @@ class VisitorChecksOutOrderTest < ApplicationSystemTestCase
     click_on submit(:shipment, :update)
   end
 
+  def assert_describedby(locator, text)
+    input = find_stripe_field(locator)
+    described_by_element = find_by_id(input["aria-describedby"])
+
+    within described_by_element do
+      assert_text text
+    end
+  end
+
   def fill_in_stripe(locator, *arguments)
-    label = find("label", text: locator)
-    input = find_by_id(label["for"])
+    input = find_stripe_field(locator)
 
     within_frame(input.find("iframe")) { fill_in(*arguments) }
+  end
+
+  def find_stripe_field(locator)
+    label = find("label", text: locator)
+
+    find_by_id(label["for"])
   end
 
   def assert_address(address)
