e0bf4923a6a03ca16321fccfc11e60a7cc010a4a
Sean Doyle <sean.p.doyle24@gmail.com>
2020-08-18 17:00:18 -0400

    [COME BACK] Stripe JS Errors
    [test-card-numbers]: https://stripe.com/docs/testing#cards-responses

--- test/system/visitor_checks_out_order_test.rb
+++ test/system/visitor_checks_out_order_test.rb
@@ -60,6 +60,51 @@ class VisitorChecksOutOrderTest < ApplicationSystemTestCase
     assert_total shipment.total_in_dollars
   end
 
+  test "visitor is notified when a payment submission is declined" do
+    shipment = shipments(:shipment_rails)
+
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0002
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_number), "Your card was declined."
+  end
+
+  test "visitor is notified when a payment submission has an expired card" do
+    shipment = shipments(:shipment_rails)
+
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0069
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_expiry), "Your card has expired."
+  end
+
+  test "visitor is notified when a payment submission's card verification check fails" do
+    shipment = shipments(:shipment_rails)
+
+    shipment.books.each { |book| add_to_cart book }
+    checkout
+    submit_shipment_form(shipment)
+    fill_in_stripe label(:payment, :card_number), with: 4000_0000_0000_0101
+    fill_in_stripe label(:payment, :card_expiry), with: 3_30
+    fill_in_stripe label(:payment, :card_cvc), with: 737
+    click_on submit(:payment, :update)
+
+    assert_no_text translate("checkouts.confirmations.show.title")
+    assert_describedby label(:payment, :card_cvc), "Your card's security code is incorrect."
+  end
+
   def add_to_cart(book, increment: 1)
     visit book_path(book)
     fill_in label(:line_item, :increment), with: increment
@@ -83,11 +128,25 @@ class VisitorChecksOutOrderTest < ApplicationSystemTestCase
     click_on submit(:shipment, :update)
   end
 
+  def assert_describedby(locator, text)
+    input = find_stripe_field(locator)
+    described_by_element = find_by_id(input["aria-describedby"])
+
+    within described_by_element do
+      assert_text text
+    end
+  end
+
   def fill_in_stripe(locator, *arguments)
-    label = find("label", text: locator)
-    input = find_by_id(label["for"])
+    input = find_stripe_field(locator)
 
     within_frame(input.find("iframe")) { fill_in(*arguments) }
+  end
+
+  def find_stripe_field(locator)
+    label = find("label", text: locator)
+
+    find_by_id(label["for"])
   end
 
   def label(i18n_key, attribute)
