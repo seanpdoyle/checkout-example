d60ad224b15ef294690ec7bec520d4bfc8d24fdd
Sean Doyle <sean.p.doyle24@gmail.com>
2020-08-29 12:45:58 -0400

    Limit confirmations#show to only charged orders

--- app/controllers/checkouts/confirmations_controller.rb
+++ app/controllers/checkouts/confirmations_controller.rb
@@ -3,7 +3,11 @@ module Checkouts
     def show
       confirmation = Order.find_signed!(params[:id])
 
-      render locals: {confirmation: confirmation}
+      if confirmation.paid?
+        render locals: {confirmation: confirmation}
+      else
+        redirect_to new_order_payment_path(confirmation)
+      end
     end
   end
 end
--- test/controllers/checkouts/confirmations_controller_test.rb
+++ test/controllers/checkouts/confirmations_controller_test.rb
@@ -2,18 +2,27 @@ require "test_helper"
 
 class ConfirmationsControllerTest < ActionDispatch::IntegrationTest
   test "#show fetches the Confirmation by its signed_id" do
-    order = orders(:rails)
+    confirmation = payments(:payment_rails).becomes(Order)
 
-    get confirmation_path(order.signed_id)
+    get confirmation_path(confirmation.signed_id)
 
     assert_response :success
   end
 
   test "#show raises ActiveRecord::RecordNotFound error when visited with id" do
-    order = orders(:rails)
+    confirmation = payments(:payment_rails).becomes(Order)
 
     assert_raises ActiveSupport::MessageVerifier::InvalidSignature do
-      get confirmation_path(order.id)
+      get confirmation_path(confirmation.id)
     end
+  end
+
+  test "#show redirects to the Payment form if not Checked out" do
+    current_order = shipments(:shipment_rails).becomes(Order)
+    cookies[:order_id] = current_order.signed_id
+
+    get confirmation_path(current_order.signed_id)
+
+    assert_redirected_to new_order_payment_url(current_order)
   end
 end
